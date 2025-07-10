eda30b6928eb20bd4227623d0e6a96c8a6404287
gtech29 <juan.rodriguez.517@my.csun.edu>
2025-07-10 09:30:39 -0700

    update env

--- src/lib/firebaseAdmin.ts
+++ src/lib/firebaseAdmin.ts
@@ -7,6 +7,7 @@ if (!getApps().length) {
     credential: cert({
       projectId: process.env.FIREBASE_PROJECT_ID,
       clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+      // Replace escaped newlines with real ones
       privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
     }),
   });
